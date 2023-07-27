0961ca3054cd6af510fbd6fef5edaa0ee1ac1814
sofc-T <129575612+sofc-T@users.noreply.github.com>
2023-07-27 06:31:49 -0700

    Committing files

---
+++ CodeForces/Python 3/1399A | Remove Smallest/214358182.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+    def xo():
+        n = int(input())
+        nums = list(map(int,input().split()))
+        nums.sort()
+        i = 1
+        while i < n:
+            if nums[i-1] == nums[i] or nums[i] == nums[i-1] + 1:
+                i += 1
+            else:
+                return "NO"
+        return "YES"
+    print(xo())
+        
+            
+            
